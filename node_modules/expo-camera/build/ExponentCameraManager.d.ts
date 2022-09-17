265dbc6743b2f0533485d03a929cb723130eed5b
Muhammad Mudassir <61446880+mudassir089@users.noreply.github.com>
2022-09-17 20:12:45 +0500

    Delete node_modules directory

--- node_modules/expo-camera/build/ExponentCameraManager.d.ts
+++
@@ -1,2 +0,0 @@
-declare const CameraManager: Record<string, any>;
-export default CameraManager;
--- node_modules/expo-gl/build/Canvas.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { ViewProps } from 'react-native';
-declare const CanvasWrapper: React.FunctionComponent<ViewProps & {
-    canvasRef: React.Ref<HTMLCanvasElement>;
-}>;
-export default CanvasWrapper;
